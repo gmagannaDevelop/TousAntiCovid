722f945cb201a550da00e47d71dcdb4db09237d2
Gustavo Magaña López <gmaganna.biomed@gmail.com>
2020-12-20 15:10:25 +0100

    :sparkles: Added new probabilities.
    src/probabilities.h now includes displacement probability and the probability of choosing a new direction over obeying momentum.

--- src/probabilities.h
+++ src/probabilities.h
@@ -7,15 +7,17 @@
     #define P_INIT_VIRUS 0.05
     
     #define P_ASYMPTOMATIC 2.0/3.0                                                  
-    /* Given the dicotomy of the events, 
-       there may not be a need for
-       P_SYMPTOMATIC
-    */
     #define P_SYMPTOMATIC 1.0 - P_ASYMPTOMATIC
 
     #define VIRULENCE 1.0/10.0
     #define MORTALITY VIRULENCE
-  
+
+    #define P_MOVE 0.70
+    #define P_STAY 1.0 - P_MOVE
+
+    #define P_MOVE_MOMENTUM 3.0/4.0
+    #define P_MOVE_RANDOM 1.0 - P_MOVE_MOMENTUM
+
 /*# Function protoypes : */
   
     /* 
